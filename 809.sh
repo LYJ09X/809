15edfb11aa9cbebabd974b64c9d2aa5722f46e3f
LYJ09X <58910154+LYJ09X@users.noreply.github.com>
2022-03-29 15:11:47 +0800

    Create 809.sh

---
+++ 809.sh
@@ -0,0 +1,13 @@
+read -p "请输入x-ui的IP: " spip
+read -p "请输入x-ui的端口: " spport
+fakeid=$(echo $RANDOM | md5sum | cut -c 1-22)
+md5="3d99ff138e1f41e931e58617e7d128e2"
+spkey=$(echo -n "if5ax/?fakeid=$fakeid&spid=81117&pid=81117&spip=$spip&spport=$spport$md5" | md5sum | cut -d " " -f1)
+url=$(curl -s -X GET -H "Host:dir.wo186.tv:809" -H "User-Agent:Mozilla/5.0 (Linux; Android 11; M2012K11AC) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.104 Mobile Safari/537.36" "http://dir.wo186.tv:809/if5ax/?fakeid=$fakeid&spid=81117&pid=81117&spip=$spip&spport=$spport&spkey=$spkey" | grep -o "url.*" | cut -d '"' -f3 | sed 's/\\//g')
+host=$(echo $url | cut -d "/" -f3)
+path=$(echo $url | grep -o "/if5ax.*")
+echo "修改代理应用
+ip与host改为 $(echo $host | cut -d ":" -f1)
+端口为 $(echo $host | cut -d ":" -f2)
+path改为
+$path"
