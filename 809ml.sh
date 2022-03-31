c28f2ae97b8c7de49347be96e75af7cd0d73ff64
LYJ09X <58910154+LYJ09X@users.noreply.github.com>
2022-03-31 23:16:22 +0800

    Delete 809ml.sh

--- 809ml.sh
+++
@@ -1,5 +0,0 @@
-mkdir lt809ml
-wget --no-check-certificate -O /root/lt809ml/sub https://cdn.jsdelivr.net/gh/LYJ09X/809@main/sub
-wget --no-check-certificate -O /root/lt809ml/sub.conf https://cdn.jsdelivr.net/gh/LYJ09X/809@main/sub.conf
-sudo chmod 644 /root/lt809ml/sub.conf
-sudo chmod 747 /root/lt809ml/sub
