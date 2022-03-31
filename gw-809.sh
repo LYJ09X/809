6c4cec2d951e63f8db932f11a285e5f90e2fcee8
LYJ09X <58910154+LYJ09X@users.noreply.github.com>
2022-03-31 23:49:47 +0800

    Update and rename gw-809ml.sh to gw-809.sh

---
+++ gw-809.sh
@@ -0,0 +1,5 @@
+mkdir lt809ml
+wget --no-check-certificate -O /root/lt809ml/sub https://raw.githubusercontent.com/LYJ09X/809/main/gw-sub
+wget --no-check-certificate -O /root/lt809ml/sub.conf https://raw.githubusercontent.com/LYJ09X/809/main/gw-sub.conf
+sudo chmod 644 /root/lt809ml/sub.conf
+sudo chmod 747 /root/lt809ml/sub
