da3dfa0cd33cdb947818888ba887d41e0ab41da5
LYJ09X <58910154+LYJ09X@users.noreply.github.com>
2022-03-31 23:56:03 +0800

    Create gn-809.sh

---
+++ gn-809.sh
@@ -0,0 +1,5 @@
+mkdir lt809ml
+wget --no-check-certificate -O /root/lt809ml/sub https://cdn.jsdelivr.net/gh/LYJ09X/809@main/gn-sub
+wget --no-check-certificate -O /root/lt809ml/sub.conf https://cdn.jsdelivr.net/gh/LYJ09X/809@main/gn-sub.conf
+sudo chmod 644 /root/lt809ml/sub.conf
+sudo chmod 747 /root/lt809ml/sub
