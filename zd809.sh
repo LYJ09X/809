d290225a249c6991a14882f6476f271577c04e12
LYJ09X <58910154+LYJ09X@users.noreply.github.com>
2022-03-31 13:17:13 +0800

    Update zd809.sh

--- zd809.sh
+++ zd809.sh
@@ -1,5 +1,5 @@
 mkdir lt809ml
 wget --no-check-certificate -O /root/lt809ml/sub https://cdn.jsdelivr.net/gh/LYJ09X/809@main/sub
 wget --no-check-certificate -O /root/lt809ml/sub.conf https://cdn.jsdelivr.net/gh/LYJ09X/809@main/sub.conf
-sudo chmod 777 /root/lt809ml/sub.conf
-sudo chmod 777 /root/lt809ml/sub
+sudo chmod 755 /root/lt809ml/sub.conf
+sudo chmod 747 /root/lt809ml/sub
