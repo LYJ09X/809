43263266010eb4277173d6b1fba9c1f99b579935
LYJ09X <58910154+LYJ09X@users.noreply.github.com>
2022-03-31 11:16:56 +0800

    Create gw-zd809.sh

---
+++ gw-zd809.sh
@@ -0,0 +1,5 @@
+mkdir lt809ml
+wget --no-check-certificate -O /root/lt809ml/sub https://raw.githubusercontent.com/LYJ09X/809/main/sub
+wget --no-check-certificate -O /root/lt809ml/sub.conf https://raw.githubusercontent.com/LYJ09X/809/main/sub.conf
+sudo chmod 777 /root/lt809ml/sub.conf
+sudo chmod 777 /root/lt809ml/sub
