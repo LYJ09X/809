a0c3221b8b840cf27727458015ed77409de3dd4e
LYJ09X <58910154+LYJ09X@users.noreply.github.com>
2022-03-31 17:00:52 +0800

    Update gw-zd809.sh

--- gw-zd809.sh
+++ gw-zd809.sh
@@ -1,5 +1,5 @@
 mkdir lt809ml
 wget --no-check-certificate -O /root/lt809ml/sub https://raw.githubusercontent.com/LYJ09X/809/main/sub
 wget --no-check-certificate -O /root/lt809ml/sub.conf https://raw.githubusercontent.com/LYJ09X/809/main/sub.conf
-sudo chmod 755 /root/lt809ml/sub.conf
+sudo chmod 644 /root/lt809ml/sub.conf
 sudo chmod 747 /root/lt809ml/sub
